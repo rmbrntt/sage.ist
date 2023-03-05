b24d138d4e21bab640f72eef2151f285edaf3d9d
Ryan Barnett <ryan@barnett.io>
2023-03-05 17:38:58 -0500

    initial commit 🚀

---
+++ src/components/Footer.jsx
@@ -0,0 +1,54 @@
+import Link from 'next/link'
+import clsx from 'clsx'
+
+import { Container } from '@/components/Container'
+import {
+  GitHubIcon,
+} from '@/components/SocialIcons'
+
+function SocialLink({ className, href, children, icon: Icon }) {
+  return (
+    <li className={clsx(className, 'flex')}>
+      <Link
+        href={href}
+        className="group flex text-sm font-medium text-zinc-800 transition hover:text-teal-500 dark:text-zinc-200 dark:hover:text-teal-500"
+      >
+        <Icon className="h-6 w-6 flex-none fill-zinc-500 transition group-hover:fill-teal-500" />
+        <span className="ml-4">{children}</span>
+      </Link>
+    </li>
+  )
+}
+
+function NavLink({ href, children }) {
+  return (
+    <Link
+      href={href}
+      className="transition hover:text-teal-500 dark:hover:text-teal-400 font-semibold"
+    >
+      {children}
+    </Link>
+  )
+}
+
+export function Footer() {
+  return (
+    <footer className="mt-32">
+      <Container.Outer>
+        <div className="border-t border-zinc-100 pt-5 pb-16 dark:border-zinc-700/40">
+          <Container.Inner>
+            <div className="flex flex-col items-center justify-between gap-6 sm:flex-row">
+              <div className="flex gap-6 text-sm font-medium text-zinc-800 dark:text-zinc-200">
+                <SocialLink href="https://github.com/rmbrntt/sage.ist" icon={GitHubIcon} className=""></SocialLink>
+              </div>
+              <p className="text-sm text-zinc-400 dark:text-zinc-500">
+                &copy; {new Date().getFullYear()} <NavLink href="https://ryanbarnett.dev">Ryan Barnett</NavLink>. All rights
+                reserved.
+              </p>
+            </div>
+          </Container.Inner>
+        </div>
+      </Container.Outer>
+    </footer>
+  )
+}
